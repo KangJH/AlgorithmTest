9761590f9bede745e905167eebb387dfee2e53e8
Jaehyun Kang <am11_00@naver.com>
2019-05-04 19:37:56 +0900

    Fix bug

--- AlgoTest/SortTest.cpp
+++ AlgoTest/SortTest.cpp
@@ -22,17 +22,17 @@ void merge(vector<int>& input, int l, int m, int r)
 	vector<int> result = {};
 	vector<int>::iterator itr_f = front.begin();
 	vector<int>::iterator itr_e = end.begin();
-	for (; itr_f != front.end() && itr_e != end.end(); itr_f++, itr_e++)
+	for (; itr_f != front.end() && itr_e != end.end(); )
 	{
 		if (*itr_f < *itr_e)
 		{
 			result.push_back(*itr_f);
-			result.push_back(*itr_e);
+			itr_f++;
 		}
 		else
 		{
 			result.push_back(*itr_e);
-			result.push_back(*itr_f);
+			itr_e++;
 		}
 	}
 
@@ -65,13 +65,23 @@ void mergeSort(vector<int>& input, int l, int r)
 
 void SortTest::MergeSort()
 {
-	vector<int> testNums = { 0, 8, 19, 3, 3, 5, 1 };
-	mergeSort(testNums, 0, testNums.size() - 1);
-	for (vector<int>::iterator itr = testNums.begin(); itr != testNums.end(); itr++)
+	vector<vector<int>> testNums = {
+		{ 0, 8, 19, 3, 3, 5, 1 } , {}, {1}, 
+		{23, 5234, 523, 64, -1, 3, 4}, 
+		{0,0,0,0,0,0,1},
+		{1,3,4,6,6,3,2,76,9,4,2,5,6,4,2,6,89,}
+	};
+	for (vector<vector<int>>::iterator itr = testNums.begin(); itr != testNums.end(); itr++)
 	{
-		cout << *itr << ",";
+		vector<int> &val = *itr;
+		mergeSort(val, 0, val.size() - 1);
+		cout << "{";
+		for (vector<int>::iterator itr2 = val.begin(); itr2 != val.end(); itr2++)
+		{
+			cout << *itr2 << ",";
+		}
+		cout << "} \n";
 	}
-	cout << "\n";
 }
 
 void SortTest::QuickSort()
